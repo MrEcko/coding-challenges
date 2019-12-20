8abaf5503943d5b8c3598ca1429874dab49fe148
Laurentiu <laurentiu.lucaci@gmail.com>
2019-12-20 20:59:40 +0100

    Replace JUnit5 with Spock because of easier data tests

---
+++ src/test/groovy/world/coding/challenges/AppTest.groovy
@@ -0,0 +1,17 @@
+package world.coding.challenges
+
+import spock.lang.Specification
+
+class AppTest extends Specification {
+
+    def "application has a greeting"() {
+        given:
+        def app = new App()
+
+        when:
+        def result = app.greeting
+
+        then:
+        result != null
+    }
+}
