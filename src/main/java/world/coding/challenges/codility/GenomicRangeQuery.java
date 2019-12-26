e6308e3c9f3dc2c799581401c259849736711b23
Laurentiu <laurentiu.lucaci@gmail.com>
2019-12-26 23:19:04 +0100

    Implemented an efficient solution to GenomicRangeQuery Based on the tip of representing each type as a vector where the index is the priority. How do you guys come up with this representation? Great challenge tho for mastering the prefix sum technique

--- src/main/java/world/coding/challenges/codility/GenomicRangeQuery.java
+++ src/main/java/world/coding/challenges/codility/GenomicRangeQuery.java
@@ -1,6 +1,9 @@
 package world.coding.challenges.codility;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * https://app.codility.com/programmers/lessons/5-prefix_sums/genomic_range_query/
@@ -18,49 +21,65 @@ public class GenomicRangeQuery {
     }
 
     /**
-     * @param S string lenght N [1..100,000] with A,C,G,T (impact 1,2,3,4)
+     * @param S string length N [1..100,000] with A,C,G,T (impact 1,2,3,4)
      * @param P array of length M [1..50,000] with values [0..N-1]
      * @param Q array of length M [1..50,000] with values [0..N-1]
      *          P[K]<=Q[K] ; 0<=0<M
      * @return array with M integers with min impact factor for each query
      */
     public int[] solution(String S, int[] P, int[] Q) {
-        // return efficientSolution(S,P,Q);
-        return bruteForceSolution(S,P,Q);
+        return efficientSolution(S, P, Q);
     }
 
     public int[] efficientSolution(String S, int[] P, int[] Q) {
-        //input validatin goes here - to do NPEs or IllegalArgumentException
-        int M = P.length;
-        int[] solution = new int[M];
-        int[] input = computeImpactFactors(S);
-        //System.out.println(Arrays.toString(input));
-        //  0  1  2  3  4  5  6
-        // [2, 1, 3, 2, 2, 4, 1]
+        int queriesCount = P.length;
+        int stringLength = S.length();
+        int numberOfTypes = NUCLEOTIDE_IMPACT_FACTORS.size();
 
+        //represent input as a matrix - each row is a type
+        // each type is a vector where the index is the impact (e.g. A with impact 1 is [1, 0, ... ,0 ])
+        int[][] inputMatrix = new int[stringLength][numberOfTypes];
+        for (int N = 0; N < stringLength; N++) {
+            int impact = NUCLEOTIDE_IMPACT_FACTORS.get(S.charAt(N));
+            inputMatrix[N][impact - 1] = 1;
+        }
+        printMatrix(inputMatrix);
 
-        // => mins
-        // need to scan it somehow
-        //[0, 2, 1, 1, 1, 1, 1, 1]
-        // [1, 1, 1, 1, 1, 1, 1, 0]
-
-        //brute force (N^2)
-        for (int k=0; k < M; k++){
-            int left = P[k];
-            int right = Q[k];
-            //[2,4], [5,5], [0,6]
-            int min = input[left];
-            for (int i=left+1; i<=right; i++){
-                if (input[i]<min){
-                    min = input[i];
+        int[][] prefixSum = new int[stringLength+1][numberOfTypes];
+        for (int i = 1; i < stringLength+1; i++) {
+            int impact = NUCLEOTIDE_IMPACT_FACTORS.get(S.charAt(i-1));
+            int impactIndex = impact-1;
+            for (int j = 0; j < numberOfTypes; j++) {
+                // P[k] = P[k-1]+a[k-1]     - except j is needed because java doesn't sum vectors directly
+                prefixSum[i][j] = prefixSum[i - 1][j] + inputMatrix[i-1][j];
+                if (j == impactIndex){
+                    prefixSum[i][j]++;
                 }
             }
-            //System.out.println("["+left+","+right+"] min is " + min);
-            solution[k] = min;
         }
-        //assumptions
-        //P.length == M.length
-        return solution; // [2,4,1]
+        System.out.println("Prefix sum");
+        printMatrix(prefixSum);
+
+        //check each slice for min by simple subtraction
+        int[] output = new int[queriesCount];
+        for (int currentQuery = 0; currentQuery < queriesCount; currentQuery++) {
+            int X = P[currentQuery];
+            int Y = Q[currentQuery];
+            for (int currentType = 0; currentType < numberOfTypes; currentType++) {
+                //total of one slice [x,y] is P[Y+1] - P[X]
+                if (prefixSum[Y+1][currentType] - prefixSum[X][currentType] > 0) {
+                    output[currentQuery] = currentType + 1; //+1 because of index solution
+                    break; // first type is minimum impact
+                }
+            }
+        }
+        return output;
+    }
+
+    private void printMatrix(int[][] matrix) {
+        for (int i=0; i<matrix.length;i ++){
+            System.out.println(Arrays.toString(matrix[i]));
+        }
     }
 
     public int[] bruteForceSolution(String S, int[] P, int[] Q) {
