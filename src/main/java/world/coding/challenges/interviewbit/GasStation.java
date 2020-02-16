1a9e668a05e5233c75147a4bae8f6b25a73ab957
Laurentiu <laurentiu.lucaci@gmail.com>
2020-02-16 18:23:15 +0100

    Added GasStation editorial solution

--- src/main/java/world/coding/challenges/interviewbit/GasStation.java
+++ src/main/java/world/coding/challenges/interviewbit/GasStation.java
@@ -23,4 +23,22 @@ public class GasStation {
         if (total < 0) return -1;
         else return start;
     }
+
+    public int canCompleteCircuitEditorial(final List<Integer> gas, final List<Integer> cost) {
+        int currentFuel = 0;
+        int remaining = 0;
+        int total = 0;
+        int start = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            remaining = gas.get(i) - cost.get(i);
+            if (currentFuel >= 0)
+                currentFuel += remaining;
+            else {
+                currentFuel = remaining;
+                start = i;
+            }
+            total += remaining;
+        }
+        return total >= 0 ? start : -1;
+    }
 }
