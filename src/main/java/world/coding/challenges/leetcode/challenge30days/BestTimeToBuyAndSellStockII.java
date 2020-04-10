c1ea56af7196a06d8e54a148fe9dd77525303d2f
Laurentiu <laurentiu.lucaci@gmail.com>
2020-04-10 15:17:48 +0200

    Fixed BestTimeToBuyAndSellStockII - the loop needs to be outside

--- src/main/java/world/coding/challenges/leetcode/challenge30days/BestTimeToBuyAndSellStockII.java
+++ src/main/java/world/coding/challenges/leetcode/challenge30days/BestTimeToBuyAndSellStockII.java
@@ -7,21 +7,32 @@ import java.util.List;
 public class BestTimeToBuyAndSellStockII {
 
     public int maxProfit(int[] prices) {
+        int maxP = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            maxP = Math.max(maxP, maxProfitUtil(Arrays.copyOfRange(prices, i, prices.length)));
+        }
+        return maxP;
+    }
+
+    public int maxProfitUtil(int[] prices) {
         int maxProfit = 0;
         int n = prices.length;
-        if (n < 2) return maxProfit;
-        for (int i=0; i<n-1;i++){
-            int buy = prices[i];
-            List<Integer> profitableSellingDays = findProfitableSellingDays(buy, prices, i+1, n);
-            if (profitableSellingDays.size() == 0) {
-                maxProfit = maxProfit(Arrays.copyOfRange(prices, i+1, n));
-            } else {
-                for (int profitableSellingDay : profitableSellingDays) {
-                    int sell = prices[profitableSellingDay];
-                    int profit = sell - buy + maxProfit(Arrays.copyOfRange(prices, profitableSellingDay + 1, n));
-                    maxProfit = Math.max(maxProfit, profit);
-                    System.out.println("[" + buy + "," + sell + "] => mP=" + maxProfit);
-                }
+        if (n < 2)
+            return maxProfit;
+        int i = 0;
+
+        maxProfit = 0;
+        int buy = prices[i];
+        List<Integer> profitableSellingDays = findProfitableSellingDays(buy, prices, i + 1, n);
+        if (profitableSellingDays.size() == 0) {
+            int[] rest = Arrays.copyOfRange(prices, i + 1, n);
+            maxProfit = maxProfitUtil(rest);
+        } else {
+            for (int profitableSellingDay : profitableSellingDays) {
+                int sell = prices[profitableSellingDay];
+                int[] rest = Arrays.copyOfRange(prices, profitableSellingDay + 1, n);
+                int profit = sell - buy + maxProfitUtil(rest);
+                maxProfit = Math.max(maxProfit, profit);
             }
         }
 
