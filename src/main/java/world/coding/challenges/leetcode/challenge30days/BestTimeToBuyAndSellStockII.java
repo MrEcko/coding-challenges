129e3270750095a9405ed078287c28dfb16f1480
Laurentiu <laurentiu.lucaci@gmail.com>
2020-04-10 14:42:54 +0200

    BestTimeToBuyAndSellStockII - missed a test case

--- src/main/java/world/coding/challenges/leetcode/challenge30days/BestTimeToBuyAndSellStockII.java
+++ src/main/java/world/coding/challenges/leetcode/challenge30days/BestTimeToBuyAndSellStockII.java
@@ -10,23 +10,27 @@ public class BestTimeToBuyAndSellStockII {
         int maxProfit = 0;
         int n = prices.length;
         if (n < 2) return maxProfit;
-        int buy = prices[0];
-        List<Integer> profitableSellingDays = findProfitableSellingDays(buy, prices, 1, n);
-        if (profitableSellingDays.size()==0){
-            maxProfit = maxProfit(Arrays.copyOfRange(prices,1,n));
-        } else{
-            for (int profitableSellingDay : profitableSellingDays){
-                int profit = prices[profitableSellingDay] - buy;
-                profit += maxProfit(Arrays.copyOfRange(prices,profitableSellingDay+1, n));
-                maxProfit = Math.max(maxProfit, profit);
+        for (int i=0; i<n-1;i++){
+            int buy = prices[i];
+            List<Integer> profitableSellingDays = findProfitableSellingDays(buy, prices, i+1, n);
+            if (profitableSellingDays.size() == 0) {
+                maxProfit = maxProfit(Arrays.copyOfRange(prices, i+1, n));
+            } else {
+                for (int profitableSellingDay : profitableSellingDays) {
+                    int sell = prices[profitableSellingDay];
+                    int profit = sell - buy + maxProfit(Arrays.copyOfRange(prices, profitableSellingDay + 1, n));
+                    maxProfit = Math.max(maxProfit, profit);
+                    System.out.println("[" + buy + "," + sell + "] => mP=" + maxProfit);
+                }
             }
         }
+
         return maxProfit;
     }
 
     private List<Integer> findProfitableSellingDays(int buy, int[] prices, int start, int end) {
         List<Integer> output = new ArrayList<>();
-        for (int i=start;i<end; i++){
+        for (int i = start; i < end; i++) {
             if (prices[i] > buy) output.add(i);
         }
         return output;
--- src/test/groovy/world/coding/challenges/leetcode/challenge30days/BestTimeToBuyAndSellStockIITest.groovy
+++ src/test/groovy/world/coding/challenges/leetcode/challenge30days/BestTimeToBuyAndSellStockIITest.groovy
@@ -13,6 +13,7 @@ class BestTimeToBuyAndSellStockIITest extends Specification {
 
         where:
         input              || expectedOut
+        [6, 1, 3, 2, 4, 7] || 7
         [7, 1, 5, 3, 6, 4] || 7
         [1, 2, 3, 4, 5]    || 4
         [7, 6, 4, 3, 1]    || 0
